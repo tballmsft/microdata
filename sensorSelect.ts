b6bca3a9b214d282991a0cbb12d230305e456036
Kier Palin <kierpalin@gmail.com>
2024-02-14 22:09:22 +0000

    sensorSelect.ts: Added 9 more options

--- sensorSelect.ts
+++ sensorSelect.ts
@@ -1,79 +1,79 @@
 namespace microcode {
     export class SensorSelect extends CursorSceneWithPriorPage {
         private selectedSensor: () => number
-
-        private selectLightSensorBtn: Button
-        private selectTemperatureBtn: Button
-        private selectAccelerometerBtn: Button
+        private btns: Button[]
 
         constructor(app: App) {
             super(app, function () {app.popScene(); app.pushScene(new Home(this.app))})
+            // super(app)
+            this.btns = []
         }
 
         /* override */ startup() {
             super.startup()
+            
+            interface btnData {
+                ariaID: string, 
+                x: number, 
+                y: number, 
+                name: string, 
+                fn: () => number
+            }
 
-            this.selectLightSensorBtn = new Button({
-                parent: null,
-                style: ButtonStyles.FlatWhite,
-                icon: "led_light_sensor",
-                ariaId: "Light Level",
-                x: -50,
-                y: 30,
-                onClick: () => {
-                    this.app.popScene()
+            /**
+             * Generalisation for touch pin events
+             * Necessary since the inner function of input.onPinPressed(pin fn(){}) is () => void; where () => number is required
+             * @param pin TouchPin.P0, TouchPin.P1, TouchPin.P2
+             * @returns 
+             */
+            const pinPressFunction = function(pin: TouchPin): number {
+                let res: number = 0
+                input.onPinPressed(TouchPin.P0, function () {
+                    res = 1
+                })
+                return res
+            }
 
-                    const opts = {
-                        sensorFn: function () {return input.lightLevel() / 255}, 
-                        sensorName: "Light\nLevel", 
-                    }
-                    this.app.pushScene(new FrequencySelect(this.app, opts))
-                },
-            })
+            const sensorBtnData: {[id: string]: btnData;} = {
+                "disk1": {ariaID: "disk1", x: -50, y: -25, name: "disk1", fn: function () {return input.compassHeading()}},
+                // "disk2": {ariaID: "disk2", x: 0, y: -25, name: "disk2", fn: function () {return input.soundLevel()}},
+                // "disk3": {ariaID: "disk3", x: 50, y: -25, name: "disk3", fn: function () {return input.magneticForce(Dimension.X)}},
 
-            this.selectTemperatureBtn = new Button({
-                parent: null,
-                style: ButtonStyles.FlatWhite,
-                icon: "thermometer",
-                ariaId: "Thermometer",
-                x: 0,
-                y: 30,
-                onClick: () => {
-                    this.app.popScene()
- 
-                    const opts = {
-                        sensorFn: function () {return input.temperature()}, 
-                        sensorName: "Temperature", 
-                    }
-                    this.app.pushScene(new FrequencySelect(this.app, opts))
-                },
-            })
+                "led_light_sensor": {ariaID: "Light Level", x: -50, y: 0, name: "Light\nLevel", fn: function () {return input.lightLevel()}},
+                "thermometer": {ariaID: "Thermometer", x: 0, y: 0, name: "Temperature", fn: function () {return input.temperature()}},
+                "accelerometer": {ariaID: "Accelerometer", x: 50, y: 0, name: "Accelerometer", fn: function () {return input.acceleration(Dimension.X)}},
 
-            this.selectAccelerometerBtn = new Button({
-                parent: null,
-                style: ButtonStyles.FlatWhite,
-                icon: "accelerometer",
-                ariaId: "Accelerometer",
-                x: 50,
-                y: 30,
-                onClick: () => {
-                    this.app.popScene()
+                // "a": {ariaID: "a", x: -50, y: 25, name: "Light\nLevel", fn: function() {return pinPressFunction(TouchPin.P0)}},
+                // "b": {ariaID: "b", x: 0, y: 25, name: "Temperature", fn: function () {return pinPressFunction(TouchPin.P1)}},
+                // "c": {ariaID: "c", x: 50, y: 25, name: "Accelerometer", fn: function () {return pinPressFunction(TouchPin.P2)}},
 
-                    const opts = {
-                        sensorFn: function () {return input.acceleration(Dimension.X)}, 
-                        sensorName: "Accelerometer", 
-                    }
-                    this.app.pushScene(new FrequencySelect(this.app, opts))
-                },
-            })
+                // "moveTiltUp": {ariaID: "d", x: -50, y: 50, name: "Pitch", fn: function () {return input.rotation(Rotation.Pitch)}},
+                // "moveTiltLeft": {ariaID: "e", x: 0, y: 50, name: "Roll", fn: function () {return Rotation.Roll}},
+                // "finger_press": {ariaID: "f", x: 50, y: 50, name: "Pin Press", fn: function () {if(input.logoIsPressed()) {return 255} return 0}}
+            }
 
-            const btns: Button[] = [this.selectLightSensorBtn, this.selectTemperatureBtn, this.selectAccelerometerBtn]
-            this.navigator.addButtons(btns)
-        }
 
-        /* override */ activate() {
-            super.activate()
-            this.color = 15
+            Object.keys(sensorBtnData).forEach(
+                key => {
+                    this.btns.push(new Button({
+                        parent: null,
+                        style: ButtonStyles.FlatWhite,
+                        icon: key,
+                        ariaId: sensorBtnData[key].ariaID,
+                        x: sensorBtnData[key].x,
+                        y: sensorBtnData[key].y,
+                        onClick: () => {
+                            this.app.popScene()
+                            this.app.pushScene(new FrequencySelect(this.app, {
+                                sensorFn: sensorBtnData[key].fn, 
+                                sensorName: sensorBtnData[key].name, 
+                            }))
+                        },          
+                    }))
+                }
+            )
+
+            this.navigator.addButtons(this.btns)
         }
 
         draw() {
@@ -84,21 +84,12 @@ namespace microcode {
                 Screen.HEIGHT,
                 0xc
             )
+            
+            screen.printCenter("Select a sensor to log", 5)
 
-            const text = "Select a\nsensor to log"
-            const half = screen.width - (font.charWidth * text.length)
-
-            Screen.print(
-                text,
-                Screen.LEFT_EDGE + half,
-                Screen.TOP_EDGE + (screen.height / 3),
-                0xb,
-                simage.font8
-            )
-
-            this.selectLightSensorBtn.draw()
-            this.selectTemperatureBtn.draw()
-            this.selectAccelerometerBtn.draw()
+            this.btns.forEach((btn) => {
+                btn.draw()
+            })
             super.draw()
         }
     }
