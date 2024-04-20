0dc4e4b0aee7b36dd285184cda73997410acb244
Tom Ball (MSR) <tball@microsoft.com>
2024-04-20 14:19:25 -0700

    remove dead code

--- app.ts
+++ app.ts
@@ -2,11 +2,6 @@ namespace microcode {
     // Auto-save slot
     export const SAVESLOT_AUTO = "sa"
 
-    export interface SavedState {
-        progdef: any
-        version?: string
-    }
-
     export class App {
         sceneManager: SceneManager
 
@@ -19,28 +14,6 @@ namespace microcode {
             // this.pushScene(new SensorSelect(this, CursorSceneEnum.LiveDataViewer))
         }
 
-        public saveBuffer(slot: string, buf: Buffer) {
-            reportEvent("app.save", { slot: slot, size: buf.length })
-            console.log(`save to ${slot}: ${buf.length}b`)
-            profile()
-            settings.writeBuffer(slot, buf)
-        }
-
-        public save(slot: string, prog: ProgramDefn) {
-            this.saveBuffer(slot, prog.toBuffer())
-        }
-
-        public load(slot: string): ProgramDefn {
-            try {
-                let buf = settings.readBuffer(slot)
-                if (buf) {
-                    return ProgramDefn.fromBuffer(new BufferReader(buf))
-                }
-            } catch (e) {
-                console.log(e)
-            }
-            return undefined
-        }
 
         public pushScene(scene: Scene) {
             this.sceneManager.pushScene(scene)
