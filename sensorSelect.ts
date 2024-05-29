ba5ed33b8920ee20adf798cc55dd9d15bea75bf9
Kier Palin <kierpalin@gmail.com>
2024-05-29 08:08:22 +0100

    sensorSelect, button & cursor: reworked so that up to 5 sensors may be selected, including up to 1 Jacdac sensor.

--- sensorSelect.ts
+++ sensorSelect.ts
@@ -1,20 +1,33 @@
 namespace microcode {
+    /** 
+     * Limit to how many sensors you may record from & read from at once. Neccessary to prevent egregious lag in live-data-viewer.
+     * Inclusively, only one Jacdac sensor may be selected at once.
+     */
+    const MAX_NUMBER_OF_SENSORS: number = 5
+    
+    /** 
+     * Starting index of contigious row of Jacdac sensors.
+     * Used to ensure that Jacdac sensors are appropriately enabled/disabled.
+     */
+    const START_OF_JACDAC_BUTTONS_INDEX: number = 14
 
     /**
      * Responsible for allowing the user to select sensors to record or view live readings from.
-     *      The user may select any number of sensors to read from simultaneously.
+     *      The user may select up to 5 sensors to read from simultaneously including 1 Jacdac sensor.
      *      These sensors are passed to either the measurement screen or the live data view
      */
     export class SensorSelect extends CursorSceneWithPriorPage {
         private btns: Button[]
-        private selectedSensorNames: string[]
+        private selectedSensorAriaIDs: string[]
         private nextSceneEnum: CursorSceneEnum
+        private jacdacSensorSelected: boolean
         
         constructor(app: App, nextSceneEnum: CursorSceneEnum) {
-            super(app, function () {app.popScene(); app.pushScene(new Home(this.app))})
+            super(app, function () {app.popScene(); app.pushScene(new Home(this.app))}) 
             this.btns = []
-            this.selectedSensorNames = []
+            this.selectedSensorAriaIDs = []
             this.nextSceneEnum = nextSceneEnum
+            this.jacdacSensorSelected = false
         }
 
         /* override */ startup() {
@@ -43,12 +56,53 @@ namespace microcode {
                     x: x,
                     y: y,
                     onClick: (button: Button) => {
-                        const index = this.selectedSensorNames.indexOf(button.ariaId)
+                        // Deletion:
+                        const index = this.selectedSensorAriaIDs.indexOf(button.ariaId)
                         if (index != -1) {
-                            this.selectedSensorNames.splice(index, 1)
+                            this.selectedSensorAriaIDs.splice(index, 1)
+    
+                            if (SensorFactory.getFromAriaID(button.ariaId).isJacdac()) {
+                                this.jacdacSensorSelected = false
+                                this.setOtherJacdacButtonsTo(true)
+                            }
+
+                            // Renable all except the Jacdac buttons:
+                            for (let i = 0; i < START_OF_JACDAC_BUTTONS_INDEX; i++) {
+                                this.btns[i].pressable = true
+                            }
                         }
-                        else {
-                            this.selectedSensorNames.push(button.ariaId)
+
+                        // Addition:
+                        else if (this.selectedSensorAriaIDs.length < MAX_NUMBER_OF_SENSORS) {
+                            if (SensorFactory.getFromAriaID(button.ariaId).isJacdac()) {
+                                if (!this.jacdacSensorSelected) {
+                                    this.selectedSensorAriaIDs.push(button.ariaId)
+                                    this.jacdacSensorSelected = true
+
+                                    this.setOtherJacdacButtonsTo(false, button)
+                                }
+                            }
+        
+                            else
+                                this.selectedSensorAriaIDs.push(button.ariaId)
+                                button.pressable = true
+                        }
+
+                        // Prevention:
+                        if (this.selectedSensorAriaIDs.length >= MAX_NUMBER_OF_SENSORS) {
+                            for (let i = 0; i < this.btns.length - 1; i++) {
+                                let buttonInUse = false
+                                for (let j = 0; j < this.selectedSensorAriaIDs.length; j++) {
+                                    
+                                    if (this.btns[i].ariaId == this.selectedSensorAriaIDs[j]) {
+                                        buttonInUse = true
+                                        break
+                                    }
+                                }
+
+                                if (!buttonInUse)
+                                    this.btns[i].pressable = false
+                            }
                         }
                     },          
                     dynamicBoundaryColorsOn: true,
@@ -66,13 +120,13 @@ namespace microcode {
                 style: ButtonStyles.Transparent,
                 icon: "green_tick",
                 ariaId: "Done",
-                x: 60,
-                y: 47,
+                x,
+                y,
                 onClick: () => {
-                    if (this.selectedSensorNames.length === 0) {
+                    if (this.selectedSensorAriaIDs.length === 0) {
                         return
                     }
-                    const sensors = this.selectedSensorNames.map((name) => SENSOR_LOOKUP_TABLE[ARIAID_TO_SENSOR_NAME_LOOKUP_TABLE[name]])
+                    const sensors = this.selectedSensorAriaIDs.map((ariaID) => SensorFactory.getFromAriaID(ariaID))
                     this.app.popScene()
                     if (this.nextSceneEnum === CursorSceneEnum.LiveDataViewer) {
                         this.app.pushScene(new LiveDataViewer(this.app, sensors))
@@ -85,7 +139,21 @@ namespace microcode {
             }))
             this.navigator.addButtons(this.btns)
         }
-        
+
+        /**
+         * Modify the mutability of all of the Jacdac buttons at once.
+         * Neccessary since only one Jacdac sensor should be selected at once.
+         * @param pressableStatus to set all Jacdac buttons to.
+         * @param buttonToIgnore Optional case that ignores the pressableStatus
+         */
+        private setOtherJacdacButtonsTo(pressableStatus: boolean, buttonToIgnore?: Button) {
+            for (let i = START_OF_JACDAC_BUTTONS_INDEX; i < this.btns.length - 1; i++)
+                this.btns[i].pressable = pressableStatus
+
+            if (buttonToIgnore) 
+                buttonToIgnore.pressable = !pressableStatus
+        }
+
         draw() {
             Screen.fillRect(
                 Screen.LEFT_EDGE,
@@ -97,9 +165,9 @@ namespace microcode {
 
             screen.printCenter("Sensor Selection", 2)
 
-            for (let i = 0; i < this.btns.length; i++) {
+            for (let i = 0; i < this.btns.length; i++)
                 this.btns[i].draw()
-            }
+
             super.draw() 
         }
     }
