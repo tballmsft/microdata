97e98869895bf4b1af5fc61ab2448a9996a8d12c
Kier Palin <kierpalin@gmail.com>
2024-07-21 23:52:31 +0100

    sensorFactory: new .getFromRadioName() so that the full name of a sensor does not need to be used when sending sensor information over radio.

--- sensorFactory.ts
+++ sensorFactory.ts
@@ -55,5 +55,33 @@ namespace microcode {
                 default                     : return new AccelerometerXSensor()
             }
         }
+
+        public static getFromRadioName(ariaID: string): Sensor {
+            switch (ariaID) {
+                case("AX")  : return new AccelerometerXSensor();
+                case("AY")  : return new AccelerometerYSensor();
+                case("AZ")  : return new AccelerometerZSensor();
+                case("P")   : return new PitchSensor();
+                case("R")   : return new RollSensor();
+                case("TP0") : return new TouchPinP0Sensor();
+                case("TP1") : return new TouchPinP1Sensor();
+                case("TP2") : return new TouchPinP2Sensor();
+                case("AP0") : return new AnalogPinP0Sensor();
+                case("AP1") : return new AnalogPinP1Sensor();
+                case("AP2") : return new AnalogPinP2Sensor();
+                case("L")   : return new LightSensor();
+                case("T")   : return new TemperatureSensor();
+                case("M")   : return new MagnetXSensor();
+                case("LP")  : return new LogoPressSensor();
+                case("V")   : return new VolumeSensor();
+                case("C")   : return new CompassHeadingSensor();
+                case("JL")  : return new JacdacLightSensor();
+                case("JM")  : return new JacdacSoilMoistureSensor();
+                case("JD")  : return new JacdacDistanceSensor();
+                case("JF")  : return new JacdacFlexSensor();
+                case("JT")  : return new JacdacTemperatureSensor();
+                default     : return new AccelerometerXSensor()
+            }
+        }
     }
 }
